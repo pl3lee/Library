6aeda5cbb7f33e8f238b078e142c54642482bf1b
pl3lee <billylee123@gmail.com>
2022-05-03 22:28:55 -0400

    added code to remove and add books

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let libraryContainer = document.querySelector("div.library");
-
+let readButtons = document.querySelectorAll("button.read");
 function Library() {
     this.contents = [];
 }
@@ -11,13 +11,21 @@ function Book(title, author, numPages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleBookRead = function() {
+    this.read = this.read ? false : true;
+}
+
 Library.prototype.addBook = function(title, author, numPages, read) {
     let newBook = new Book(title, author, numPages, read);
     this.contents.push(newBook);
+    this.displayBooks();
 }
 
 Library.prototype.displayBooks = function() {
-    this.contents.forEach(function(book) {
+    // removes original cards
+    libraryContainer.replaceChildren();
+    // displays the actual books
+    this.contents.forEach(function(book, index) {
         let displayTitle = book.title;
         let displayAuthor = book.author;
         let displayNumPages = book.numPages;
@@ -44,24 +52,75 @@ Library.prototype.displayBooks = function() {
         let newCardNumPages = document.createElement("div");
         newCardNumPages.classList.add("card-content");
         newCardNumPages.setAttribute("id", "numPages");
-        newCardNumPages.textContent = displayNumPages;
+        newCardNumPages.textContent = displayNumPages.toString() + " pages";
 
         // adds read
         let newCardRead = document.createElement("div");
         newCardRead.classList.add("card-content");
         newCardRead.setAttribute("id", "read");
+        let newCardReadButton = document.createElement("button");
+        newCardReadButton.classList.add("read");
+        newCardReadButton.textContent = (displayRead) ? "Read" : "Not read";
+       
+        newCardReadButton.setAttribute("id", displayRead ? "true" : "false");
         
-        newCardRead.textContent = displayRead;
+        newCardRead.appendChild(newCardReadButton);
+
+        let newCardRemove = document.createElement("div");
+        newCardRemove.classList.add("card-content");
+        newCardRemove.setAttribute("id", "read");
+        let newCardRemoveButton = document.createElement("button");
+        newCardRemoveButton.classList.add("remove");
+        newCardRemoveButton.textContent = "Remove";
+        newCardRemove.appendChild(newCardRemoveButton);
 
 
+        newCard.setAttribute("data-bookIndex", index);
         newCard.appendChild(newCardTitle);
         newCard.appendChild(newCardAuthor);
         newCard.appendChild(newCardNumPages);
         newCard.appendChild(newCardRead);
+        newCard.appendChild(newCardRemove);
+        
         libraryContainer.appendChild(newCard);
-    })
+    });
+    let addBookCard = document.createElement("div");
+    addBookCard.classList.add("card");
+    addBookCard.setAttribute("id", "add-book");
+    let addBookIcon = document.createElement("span")
+    addBookIcon.classList.add("material-icons");
+    addBookIcon.textContent = "add";
+    let addBookText = document.createElement("p");
+    addBookText.textContent = "Add book";
+    addBookCard.appendChild(addBookIcon);
+    addBookCard.appendChild(addBookText);
+
+    libraryContainer.appendChild(addBookCard);
 }
 
+Library.prototype.removeBook = function(index) {
+    this.contents.splice(index, 1);
+    this.displayBooks();
+}
+
+Library.prototype.toggleBookRead = function(index) {
+    this.contents[index].toggleBookRead();
+    this.displayBooks();
+}
+
+// readButtons.forEach(function(element) {
+//     element.addEventListener('click', event => {
+//         console.log("clicked")
+//         myLibrary.toggleBookRead(event.target.id);
+//         this.displayBooks();
+//     });
+// });
+
+
+
+
+
 let myLibrary = new Library();
-myLibrary.addBook("testTitle", "test Author", "testPages", true)
-myLibrary.displayBooks();
+myLibrary.addBook("testTitle", "test Author", 1234, false)
+myLibrary.addBook("testTitle", "test Author", 12345, false)
+// myLibrary.removeBook(0);
